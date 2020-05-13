13390c40bcbcdb04b9d0c6cacc3a1dff3196a4cd
Mateusz Białowąs <58574619+mateuszbialowas@users.noreply.github.com>
2020-05-13 21:43:12 +0200

    Revert "added TestClass"
    This reverts commit 22ef780b7e689b9723218f3926a678e499fe642d.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,5 @@ public class Main {
         System.out.println("Działa Lukasz");
         System.out.println("Jestem ciekawy czy przez github Desktop też zadziała");
         System.out.println("Masz commenta");
-        TestClass klaska = new TestClass();
-        klaska.tester();
     }
 }
--- src/com/company/TestClass.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class TestClass {
-    public void tester(){
-        System.out.println("Wywolanie testowej klasy");
-    }
-}
